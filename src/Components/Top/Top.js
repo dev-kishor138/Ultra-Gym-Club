d4b9b783a0a66106ba349f514345e23e6bd002ea
Kishor Mahmud <dev.kishor138@gmail.com>
2022-12-09 22:56:04 +0600

    hello

--- src/Components/Top/Top.js
+++ src/Components/Top/Top.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Top = () => {
-    const [show, setShow] = useState(false)
-
-    function clickHandle(){
-        setShow(!show)
-    }
     return (
         <div className='heading'>
             <h1><i class="fa-solid fa-dumbbell"></i> Ultra Active Club</h1>
-            <button className={show && 'show'} onClick={() => clickHandle()}><i class="fa-solid fa-bars"></i></button>
             <h3>Select today’s exercise</h3>
         </div>
         
