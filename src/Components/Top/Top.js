e85a6fd57708601ee20df989892a6f50c4571220
Kishor Mahmud <dev.kishor138@gmail.com>
2022-12-09 22:40:21 +0600

    third-commit

--- src/Components/Top/Top.js
+++ src/Components/Top/Top.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Top = () => {
+    const [show, setShow] = useState(false)
+
+    function clickHandle(){
+        setShow(!show)
+    }
     return (
         <div className='heading'>
             <h1><i class="fa-solid fa-dumbbell"></i> Ultra Active Club</h1>
+            <button className={show && 'show'} onClick={() => clickHandle()}><i class="fa-solid fa-bars"></i></button>
             <h3>Select today’s exercise</h3>
         </div>
+        
     );
 };
 
